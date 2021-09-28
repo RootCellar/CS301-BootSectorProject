8ca8a508c682efe8c9029514bbad00a5c67a999d
RootCellar <RootCellar9877@GMail.com>
2021-09-27 23:19:25 -0800

    work on loading data from the disk

--- boot.asm
+++ boot.asm
@@ -1,15 +1,45 @@
 BITS 16 ; everything here is 16 bit code
 ;  Code gets loaded by the PC BIOS into address 0x7C00 and executed.
-	mov al,'H'
+	; set up a stack - 8 kilobytes
+	mov ax, 0x7C00 + 544
+	mov ss, ax
+	mov sp, 8192
+
+	mov al,'B'
 	mov ah,0x0e ; print command
 	int 0x10  ; talk to video card
 
-	mov al,'i'
-	mov ah,0x0e ; print command
-	int 0x10  ; talk to video card
+	; read the disk into 0x9000
+	mov bx, 0x9000 ; where to put the loaded data
+	mov dh, 16 ; sectors to read
+
+	push dx
+	mov ah, 0x02
+	mov cl, 2 ; 2nd sector
+	mov ch, 0 ; cylinder
+	mov dh, 0 ; head number
+	int 0x13 ; interrupt BIOS and read the data
+	jc hang ; jump to hang if there was a problem
+
+	pop dx
+
+	mov al, 'S' ; success!
+
+	; jump to it
+	jmp bx
+
+	jmp hang
+
+print_char:
+	mov ah, 0x0e
+	int 0x10
+	ret
 
 hang:
-	jmp hang
+	mov al, 'H'
+	call print_char
+hang2:
+	jmp hang2
 
 ; Pad data out to magic boot sector identifier
 times 512-2-($-$$) db 0  
